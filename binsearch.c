7b2dc02f5e7313a7d60aba929cb45a24eb846c02
HuseynIsmayilzada <hismayilzada06@gmail.com>
2024-05-08 11:37:31 -0700

    Binary Search C
    Not finished, to be implemented

---
+++ binsearch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sortArray(int *array)
+{
+  //to be implemented
+}
+
+int binsearch(/*int number, int *array[]*/)
+{
+
+    while (1)
+    {
+        printf("test");
+    }
+    return 0;
+}
+
+int main()
+{
+    int array[] = {3, 1, 2, 5, 4, 9};
+    return 0;
+}
